42d8d01061a6d043eb552724f1603dfdf4102c6a
Zachary Walters <zachary0191@gmail.com>
2013-10-17 12:56:52 -0400

    Added links for a menu

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Cse247::Application.routes.draw do
   get 'project_files/progress_report'
 
   root to: 'project-files#proposal'
+
+  match 'home', to: 'project_files#proposal'
+  match 'progress_report_1', to: 'project_files#progress_report'
 end
